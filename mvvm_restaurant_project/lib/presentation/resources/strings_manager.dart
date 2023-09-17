1d9e5227d397d2b28661254e5daa361ccacbfd25
konnofuente <konomelifuente@gmail.com>
2023-09-17 10:43:01 +0100

    added all images resources with pubspec.yaml update
    added svg format of images in the assets/image file

--- mvvm_restaurant_project/lib/presentation/resources/strings_manager.dart
+++ mvvm_restaurant_project/lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,12 @@
 class AppStrings {
   static const String noRouteFound = "No Route Found";
+  static const String onBoardingTitle1 = "The best course for you";
+  static const String onBoardingTitle2 = "The best course for you";
+  static const String onBoardingTitle3 = "The best course for you";
+  static const String onBoardingTitle4 = "The best course for you";
+
+  static const String onBoardingSubTitle1 = "The best course for you";
+  static const String onBoardingSubTitle2 = "The best course for you";
+  static const String onBoardingSubTitle3 = "The best course for you";
+  static const String onBoardingSubTitle4 = "The best course for you";
 }
