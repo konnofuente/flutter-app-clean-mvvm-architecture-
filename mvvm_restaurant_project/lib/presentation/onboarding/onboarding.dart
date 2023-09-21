295609c1da078c05f2ac69a7721f763e04e82c9b
konnofuente <konomelifuente@gmail.com>
2023-09-21 13:34:53 +0100

    rought implementation of the onboarding Screen
    Here we succeeded in implementing the onboarding screen with key features such next screen and previouse and also the splash images , title and subtitle

--- mvvm_restaurant_project/lib/presentation/onboarding/onboarding.dart
+++ mvvm_restaurant_project/lib/presentation/onboarding/onboarding.dart
@@ -1,5 +1,14 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:mvvm_restaurant_project/presentation/resources/color_manager.dart';
+import 'package:mvvm_restaurant_project/presentation/resources/route_manager.dart';
+import 'package:mvvm_restaurant_project/presentation/resources/value_manager.dart';
+import 'package:mvvm_restaurant_project/test.dart';
+
+import '../resources/assets_manager.dart';
+import '../resources/strings_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
@@ -9,11 +18,187 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(AppStrings.onBoardingSubTitle1,
+            AppStrings.onBoardingSubTitle1, ImageAssets.onboardingLogo1),
+        SliderObject(AppStrings.onBoardingSubTitle2,
+            AppStrings.onBoardingSubTitle2, ImageAssets.onboardingLogo2),
+        SliderObject(AppStrings.onBoardingSubTitle3,
+            AppStrings.onBoardingSubTitle3, ImageAssets.onboardingLogo3),
+        SliderObject(AppStrings.onBoardingSubTitle4,
+            AppStrings.onBoardingSubTitle4, ImageAssets.onboardingLogo4)
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: Center(child: Text("welcome to boarding")),
+      appBar: AppBar(
+        backgroundColor: ColorManager.white,
+        elevation: AppSize.s0,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
+      ),
+      body: PageView.builder(
+          controller: _pageController,
+          itemCount: _list.length,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+            return OnBoardingPage(_list[index]);
+          }),
+
+      //permit us to add a small bottom layer for the next page
+      bottomSheet: Container(
+        color: ColorManager.white,
+        height: AppSize.s100,
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.loginRoute);
+                  },
+                  child: Text(
+                     AppStrings.skip,
+                    style: Theme.of(context).textTheme.subtitle2,
+                    textAlign: TextAlign.end,
+                  ),
+                )),
+            // add layout for indicator and arrows
+
+            _getBottomSheetWidget(),
+          ],
+        ),
+      ),
     );
   }
+
+  Widget _getBottomSheetWidget() {
+    return Container(
+      color: ColorManager.primary,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                  height: AppSize.s20,
+                  width: AppSize.s20,
+                  child: SvgPicture.asset(ImageAssets.left_arrow_ic)),
+              onTap: () {
+                _pageController.animateToPage(_getPreviousIndex(),
+                    duration: Duration(milliseconds: DurationConstant.d300),
+                    curve: Curves.bounceInOut);
+                //function to move to previous page
+              },
+            ),
+          ),
+          Row(
+            children: [
+              for (int i = 0; i < _list.length; i++)
+                Padding(
+                  padding: EdgeInsets.all(AppPadding.p8),
+                  child: _getProperCircle(i),
+                )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(AppPadding.p14),
+            child: GestureDetector(
+              child: SizedBox(
+                  height: AppSize.s20,
+                  width: AppSize.s20,
+                  child: SvgPicture.asset(ImageAssets.right_arrow_ic)),
+              onTap: () {
+                //function to move to next page
+                _pageController.animateToPage(_getNextIndex(),
+                    duration: Duration(milliseconds: DurationConstant.d300),
+                    curve: Curves.bounceInOut);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  int _getPreviousIndex() {
+    int previousIndex = _currentIndex--;
+    // this will create an infinty loop that is when the previous is -1 it move to the last index of the screen list
+    previousIndex == -1 ? _currentIndex = _list.length - 1 : null;
+
+    return _currentIndex;
+  }
+
+  int _getNextIndex() {
+    int nextIndex = _currentIndex++;
+    // this will create an infinty loop that is when the next is 0 it move to the first index of the screen list
+    nextIndex >= _list.length ? _currentIndex = 0 : null;
+
+    return _currentIndex;
+  }
+
+  Widget _getProperCircle(int index) {
+    return SvgPicture.asset(index == _currentIndex // -1
+        ? ImageAssets.hollow_cirlce_ic
+        : ImageAssets.solid_circle_ic);
+  }
+}
+
+class OnBoardingPage extends StatelessWidget {
+  SliderObject _sliderObject;
+
+  OnBoardingPage(this._sliderObject, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        SizedBox(height: AppSize.s40),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.title,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.headline1,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(AppPadding.p8),
+          child: Text(
+            _sliderObject.subTitle,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.subtitle1,
+          ),
+        ),
+        SizedBox(
+          height: AppSize.s60,
+        ),
+        SvgPicture.asset(_sliderObject.image)
+        // image widget
+      ],
+    );
+  }
+}
+
+class SliderObject {
+  String title;
+  String subTitle;
+  String image;
+
+  SliderObject(this.title, this.subTitle, this.image);
 }
--- mvvm_restaurant_project/lib/presentation/resources/strings_manager.dart
+++ mvvm_restaurant_project/lib/presentation/resources/strings_manager.dart
@@ -1,5 +1,6 @@
 class AppStrings {
   static const String noRouteFound = "No Route Found";
+  static const String skip = "Skip";
   static const String onBoardingTitle1 = "The best course for you";
   static const String onBoardingTitle2 = "The best course for you";
   static const String onBoardingTitle3 = "The best course for you";
