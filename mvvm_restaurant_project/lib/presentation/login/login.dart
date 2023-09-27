befd35c630f28a64546daa128c70ce022cbc87dd
konnofuente <konomelifuente@gmail.com>
2023-09-27 01:34:54 +0100

    complete onBoarding

--- mvvm_restaurant_project/lib/presentation/login/login.dart
+++ mvvm_restaurant_project/lib/presentation/login/login.dart
@@ -10,6 +10,10 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+       child: Text('Login screen')
+      ),
+    );
   }
 }
